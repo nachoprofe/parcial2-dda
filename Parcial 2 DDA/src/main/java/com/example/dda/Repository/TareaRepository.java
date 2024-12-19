7906dd72e318b3823a2fc6f809a5b77dbd4ae5ed
Nacho <joseignaciooneto@gmail.com>
2024-12-19 11:27:18 -0300

    Delete Parcial 2 DDA directory

--- Parcial 2 DDA/src/main/java/com/example/dda/Repository/TareaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.dda.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.dda.Entity.Tarea;
-
-public interface TareaRepository extends JpaRepository<Tarea, Long> {
-    // Write code here
-}
