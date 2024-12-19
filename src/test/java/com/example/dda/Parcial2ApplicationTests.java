3cd6216a229033dd058b66c49c9e82e4ef8d5353
Nacho <joseignaciooneto@gmail.com>
2024-12-19 11:28:20 -0300

    Add files via upload

---
+++ src/test/java/com/example/dda/Parcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
